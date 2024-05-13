8a865bd837dcceef4534209e2a9b92ca2146b3af
Shadip Kumar Joshi <220049@softwarica.edu.np>
2024-05-13 15:52:59 +0545

    version

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,6 +1,3 @@
-import 'dart:async';
-
-import 'package:final_assignment/screen/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -14,22 +11,39 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 20), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const LoginScreen()),
-      );
-    });
+    // Timer(const Duration(seconds: 20), () {
+    //   Navigator.pushReplacement(
+    //     context,
+    //     MaterialPageRoute(builder: (context) => const LoginScreen()),
+    //   );
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Image.asset(
-          'assets/images/splash.gif',
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/splash.gif',
+                ),
+                const SizedBox(height: 50),
+                const Text(
+                  'v. 1.00',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
