dfe0822a3ec5f5baacc8c7b01ae274d4786c936f
Shadip Kumar Joshi <220049@softwarica.edu.np>
2024-05-14 11:55:58 +0545

    model preparation

---
+++ lib/model/onboarding_content_model.dart
@@ -0,0 +1,25 @@
+class UnbordingContent {
+  String image;
+  String title;
+  String discription;
+
+  UnbordingContent({this.image, this.title, this.discription});
+}
+
+List<UnbordingContent> contents = [
+  UnbordingContent(
+      title: 'Design Your Idea',
+      image: 'assets/images/onboard1.svg',
+      discription:
+          "Bring your idea to life. Design your own t-shirts and wear them with pride. Let your creativity shine!"),
+  UnbordingContent(
+      title: 'Submit Your Art',
+      image: 'assets/images/onboard1.svg',
+      discription:
+          "Art you an artist. Submit your own art for others to choose from. Share your unique style with the world!"),
+  UnbordingContent(
+      title: 'Earn Royalty',
+      image: 'assets/images/onboard1.svg',
+      discription:
+          "Earn money every time your art is printed. Turn your passion into profit!"),
+];
