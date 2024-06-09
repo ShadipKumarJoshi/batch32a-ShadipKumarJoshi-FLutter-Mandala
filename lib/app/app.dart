a7ac77ba93b57a6ed8a16496d63f0c3bc4208201
Shadip Kumar Joshi <220049@softwarica.edu.np>
2024-06-09 20:33:00 +0545

    app folder  updatae

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,18 +1,23 @@
-import 'package:final_assignment/screen/splash_screen.dart';
-import 'package:final_assignment/theme/theme_data.dart';
-import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+import 'package:final_assignment/app/navigator_key/navigator_key.dart';
+import 'package:final_assignment/app/themes/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: getApplicationTheme(),
-      // themes
-
-      home: SplashScreen(),
+      navigatorKey: AppNavigator.navigatorKey,
+      title: 'Mandala T-On-Line Prints',
+      theme: AppTheme.getApplicationTheme(false),
+      home: const SplashView(),
+      
+      // --- routes
+      // home: SplashScreen(),
       // home: LoginScreen(),
       // home: RegisterScreen(),
       // home: OnbordingScreen(),
---
+++ lib/app/constants/hive_table_constant.dart
@@ -0,0 +1,5 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+ 
+}
