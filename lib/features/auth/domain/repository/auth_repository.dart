1f067f30acf31bf16c27c5e312fea57021a83a05
Shadip Kumar Joshi <220049@softwarica.edu.np>
2024-07-04 19:46:06 +0545

    internet connection

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:final_assignment/core/common/provider/internet_connectivity.dart';
 import 'package:final_assignment/core/failure/failure.dart';
 import 'package:final_assignment/features/auth/data/repository/auth_local_repository.dart';
 import 'package:final_assignment/features/auth/data/repository/auth_remote_repository.dart';
@@ -6,7 +7,19 @@ import 'package:final_assignment/features/auth/domain/entity/auth_entity.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 final authRepositoryProvider = Provider<IAuthRepository>((ref) {
   return ref.read(authRemoteRepositoryProvider);
+
+ // Check internet connectivity
+  final checkConnectivity = ref.read(connectivityStatusProvider);
+
+  if(checkConnectivity == ConnectivityStatus.isConnected){
+    return ref.read(authRemoteRepositoryProvider);
+  }else{
+    return ref.read(authLocalRepositoryProvider);
+  }
+
 });
+
+
 
 abstract class IAuthRepository {
   Future<Either<Failure, bool>> registerUser(AuthEntity user);
