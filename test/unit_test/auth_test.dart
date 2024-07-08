30e4445adc0c2990ed7058f359076069468c75cd
Shadip Kumar Joshi <220049@softwarica.edu.np>
2024-07-08 11:44:29 +0545

    register unit testing complete

--- test/unit_test/auth_test.dart
+++ test/unit_test/auth_test.dart
@@ -14,10 +14,9 @@
 // Act: call the loginUser() method
 // Assert: check for the error in authState
 
-
-
 import 'package:dartz/dartz.dart';
 import 'package:final_assignment/core/failure/failure.dart';
+import 'package:final_assignment/features/auth/domain/entity/auth_entity.dart';
 import 'package:final_assignment/features/auth/domain/usecases/auth_usecase.dart';
 import 'package:final_assignment/features/auth/presentation/navigator/login_navigator.dart';
 import 'package:final_assignment/features/auth/presentation/navigator/register_navigator.dart';
@@ -29,16 +28,13 @@ import 'package:mockito/mockito.dart';
 
 import 'auth_test.mocks.dart';
 
-
-
 @GenerateNiceMocks([
   MockSpec<AuthUseCase>(),
   MockSpec<LoginViewNavigator>(),
   MockSpec<RegisterViewNavigator>()
 ])
 void main() {
-
-TestWidgetsFlutterBinding.ensureInitialized();
+  TestWidgetsFlutterBinding.ensureInitialized();
 
   late AuthUseCase mockAuthUsecase;
   late ProviderContainer container;
@@ -52,13 +48,13 @@ TestWidgetsFlutterBinding.ensureInitialized();
 
     container = ProviderContainer(overrides: [
       authViewModelProvider.overrideWith(
-        (ref) => AuthViewModel(mockLoginViewNavigator, mockAuthUsecase, mockRegisterViewNavigator),
+        (ref) => AuthViewModel(
+            mockLoginViewNavigator, mockAuthUsecase, mockRegisterViewNavigator),
       )
     ]);
   });
 
   test('check for the inital state in Auth state', () {
-
     // NOT (ref).read(authViewModelProvider) but container.read(authViewModelProvider) as we are using Container
     // run the test individiually and then write further
     final authState = container.read(authViewModelProvider);
@@ -67,7 +63,7 @@ TestWidgetsFlutterBinding.ensureInitialized();
     expect(authState.imageName, isNull);
   });
 
-  // ------------- LOGIN TESTS ----------------
+  // TEST 1 ------------- LOGIN TESTS ----------------
   // LOGIN TEST FOR SINGLE USER
 
   test('login test with valid emailOrPhoneNumber and password', () async {
@@ -75,7 +71,8 @@ TestWidgetsFlutterBinding.ensureInitialized();
     when(mockAuthUsecase.loginUser('shadip@shadip.com', 'shadip123'))
         .thenAnswer((_) => Future.value(const Right(true)));
 
-    when(mockAuthUsecase.loginUser('shadip@shadip.com', 'shadip1234')).thenAnswer(
+    when(mockAuthUsecase.loginUser('shadip@shadip.com', 'shadip1234'))
+        .thenAnswer(
       (_) => Future.value(
         Left(
           Failure(error: 'Invalid'),
@@ -94,14 +91,13 @@ TestWidgetsFlutterBinding.ensureInitialized();
     expect(authState.error, isNull);
   });
 
-
   // LOGIN FOR MULTIPLE USERS
   test('login test with valid emailOrPhoneNumber and password', () async {
     // Arrange
-    const correctEmailOrPhoneNumber= 'shadip@shadip.com';
+    const correctEmailOrPhoneNumber = 'shadip@shadip.com';
     const correctPassword = 'shadip123';
 
-// for (any, any) make auth_usecase (emailOrPhoneNumber and password as nullable wih ?)
+// NOTE: for (any, any) make auth_usecase (emailOrPhoneNumber and password as nullable wih ?)
     when(mockAuthUsecase.loginUser(any, any)).thenAnswer((invocation) {
       final email = invocation.positionalArguments[0] as String;
       final password = invocation.positionalArguments[1] as String;
@@ -122,10 +118,49 @@ TestWidgetsFlutterBinding.ensureInitialized();
     expect(authState.error, isNull);
   });
 
-  test('Register User', () {});
+  // TEST 2 ------------- REGISTER TESTS ----------------
+  test(
+    'Register new user test with all details test',
+    () async {
+      // Arrange
+// NOTE: for (any) make auth_usecase (AuthEntity as nullable wih ?)
+// COmment the snackbar in auth_view_model.dart and common
+      when(mockAuthUsecase.registerUser(any)).thenAnswer((innovation) {
+        final auth = innovation.positionalArguments[0] as AuthEntity;
 
+        return Future.value(
+          auth.fullname.isNotEmpty &&
+                  auth.username.isNotEmpty &&
+                  auth.phoneNumber.isNotEmpty &&
+                  auth.phoneNumber.length == 10 &&
+                  auth.email.isNotEmpty &&
+                  auth.email.contains('@') &&
+                  auth.email.contains('.') &&
+                  auth.password.isNotEmpty
+              ? const Right(true)
+              : Left(
+                  Failure(error: 'Invalid details'),
+                ),
+        );
+      });
+      // Act
+      await container
+          .read(authViewModelProvider.notifier)
+          .registerUser(const AuthEntity(
+            fullname: 'Test Test',
+            username: 'test123',
+            phoneNumber: '1234567890',
+            email: 'test@test.com',
+            password: '12345678',
+          ));
 
+      final state = container.read(authViewModelProvider);
 
+      // Assert
+      expect(state.isLoading, false);
+      expect(state.error, null);
+    },
+  );
 
 // dispose the container from   provider
   tearDown(
@@ -133,6 +168,4 @@ TestWidgetsFlutterBinding.ensureInitialized();
       container.dispose();
     },
   );
-
 }
-
