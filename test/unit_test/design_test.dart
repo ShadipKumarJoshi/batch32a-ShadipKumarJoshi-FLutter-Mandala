703ed7ab04e8fd01a020d43e927711a8be2b7bad
Shadip Kumar Joshi <220049@softwarica.edu.np>
2024-07-11 17:05:23 +0545

    pagination of design unit_testing

--- test/unit_test/design_test.dart
+++ test/unit_test/design_test.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:final_assignment/core/failure/failure.dart';
 import 'package:final_assignment/features/design/domain/entity/design_entity.dart';
 import 'package:final_assignment/features/design/domain/usecases/design_usecase.dart';
 import 'package:final_assignment/features/design/presentation/viewmodel/design_view_model.dart';
@@ -38,6 +39,61 @@ void main() {
   test('check Design initial state', () async {
     when(mockDesignUsecase.getPaginationDesigns(1, 2))
         .thenAnswer((_) => Future.value(Right(lstDesigns)));
+  });
+
+  //  test('check Design initial state', () {
+  //   final designViewModel = container.read(designViewModelProvider.notifier);
+  //   final designState = container.read(designViewModelProvider);
+
+  //   expect(designState, equals(DesignState.initial()));
+  // });
+
+// TEST 1
+  test('fetchDesigns updates state correctly on success', () async {
+    when(mockDesignUsecase.getPaginationDesigns(1, 12))
+        .thenAnswer((_) async => Right(lstDesigns));
+
+// fetch all designs
+    await container.read(designViewModelProvider.notifier).fetchDesigns();
+
+// store the state
+    final designState = container.read(designViewModelProvider);
+
+// check the state
+    expect(designState.designs, equals(lstDesigns));
+    expect(designState.page, equals(1));
+    expect(designState.isLoading, equals(false));
+    expect(designState.hasReachedMax, equals(false));
+  });
+
+// TEST 2
+  test('fetchDesigns updates state correctly on empty data', () async {
+    when(mockDesignUsecase.getPaginationDesigns(1, 12))
+        .thenAnswer((_) async => const Right([]));
+
+    await container.read(designViewModelProvider.notifier).fetchDesigns();
+
+    final designState = container.read(designViewModelProvider);
+
+    expect(designState.designs, equals([]));
+    expect(designState.page, equals(0));
+    expect(designState.isLoading, equals(false));
+    expect(designState.hasReachedMax, equals(true));
+  });
+
+// TEST 3
+  test('fetchDesigns updates state correctly on failure', () async {
+    final failure = Failure(error: "Failed to fetch designs");
+    when(mockDesignUsecase.getPaginationDesigns(1, 12))
+        .thenAnswer((_) async => Left(failure));
+
+    await container.read(designViewModelProvider.notifier).fetchDesigns();
+
+    final designState = container.read(designViewModelProvider);
+
+    expect(designState.error, equals(failure.error));
+    expect(designState.isLoading, equals(false));
+    expect(designState.hasReachedMax, equals(true));
   });
 
 // dispose the container from   provider
