12c5f60b84eccb4024c2bea5a5477b6b8726d4e9
Shadip Kumar Joshi <220049@softwarica.edu.np>
2024-08-17 01:24:55 +0545

    order unit test

--- lib/features/auth/domain/usecases/auth_usecase.dart
+++ lib/features/auth/domain/usecases/auth_usecase.dart
@@ -17,21 +17,21 @@ class AuthUseCase {
   //   return await _authRepository.uploadProfilePicture(file);
   // }
 
-  Future<Either<Failure, bool>> registerUser(AuthEntity user) async {
-    return await _authRepository.registerUser(user);
+  // Future<Either<Failure, bool>> registerUser(AuthEntity user) async {
+  //   return await _authRepository.registerUser(user);
 
     // TEST NOTE --- nullable value for register testing
-    //  Future<Either<Failure, bool>> registerUser(AuthEntity? user) async {
-    // return await _authRepository.registerUser(user!);
+     Future<Either<Failure, bool>> registerUser(AuthEntity? user) async {
+    return await _authRepository.registerUser(user!);
   }
 
-  Future<Either<Failure, bool>> loginUser(String email, String password) async {
-    return await _authRepository.loginUser(email, password);
+  // Future<Either<Failure, bool>> loginUser(String email, String password) async {
+  //   return await _authRepository.loginUser(email, password);
 
 // TEST NOTE --- nullable value for  login testing
 
-    // Future<Either<Failure, bool>> loginUser(String? email, String? password) async {
-    // return await _authRepository.loginUser(email!, password!);
+    Future<Either<Failure, bool>> loginUser(String? email, String? password) async {
+    return await _authRepository.loginUser(email!, password!);
   }
 
   Future<Either<Failure, AuthEntity>> getCurrentUser() async {
